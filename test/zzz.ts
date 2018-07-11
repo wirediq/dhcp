41afbf39d21b54a0ebfdb832b252c06c8ffa26f8
microshine <microshine@mail.ru>
2018-07-11 10:16:59 +0300

    Add connection closing script

---
+++ test/zzz.ts
@@ -0,0 +1,11 @@
+// NOTE: This test is needed for all connections closing
+
+describe("Close all connections", () => {
+  after(() => {
+    process.exit();
+  });
+
+  it("success", () => {
+    // nothing
+  });
+});
